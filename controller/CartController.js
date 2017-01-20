b04310b0663706001f8b9e1b7ff45729672a8966
bmeritter <bme_ritter@foxmail.com>
2017-01-20 15:39:21 +0800

    cart category item controller

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,11 +1,71 @@
-import Category from '../model/Category';
+import Cart from '../model/Cart';
+import Item from '../model/Item';
+import async from 'async';
 
-export default class CategoryController {
+export default class CartController {
   getAll(req, res, next) {
-    Category.find((err, doc) => {
+
+    async.waterfall([
+      (done) => {
+        Cart.find(done);
+      },
+      (data, done) => {
+        async.map(data[0].items, (item, cb) => {
+          Item.findById(item, (err, doc) => {
+            cb(null, doc);
+          });
+        }, done);
+      }], (err, data) => {
       if (err)
         return next(err);
-      res.status(200).send(doc);
+      res.send(data);
+    });
+  }
+
+  getCart(req, res, next) {
+    const cartId = req.params.cartId;
+    async.waterfall([
+      (done) => {
+        Cart.findById(cartId, done);
+      },
+      (data, done) => {
+        async.map(data.items, (item, cb) => {
+          Item.findById(item, (err, doc) => {
+            cb(null, doc);
+          });
+        }, done);
+      }], (err, data) => {
+      if (err)
+        return next(err);
+      res.send(data);
+    });
+  }
+
+  deleteCart(req, res, next) {
+    const cartId = req.params.cartId;
+    Cart.remove({'_id': cartId}, (err, doc) => {
+      if (err)
+        return next(err);
+      res.status(204).send(doc);
+    });
+  }
+
+  addCart(req, res, next) {
+    const data = req.body;
+    new Cart(data).save((err, doc) => {
+      if (err)
+        return next(err);
+      res.status(201).send(doc);
+    });
+  }
+
+  updateCart(req, res, next) {
+    const cartId = req.params.cartId;
+    const items = req.body.items;
+    Cart.update({'_id': cartId}, {items}, (err, doc) => {
+      if (err)
+        return next(err);
+      res.status(204).send(doc);
     });
   }
 
--- controller/CategoryController.js
+++ controller/CategoryController.js
@@ -1,49 +1,72 @@
+import async from 'async';
+import Category from '../model/Category';
 import Item from '../model/Item';
 
-export default class ItemController {
+export default class CategoryController {
   getAll(req, res, next) {
-    Item.find((err, doc) => {
+    async.waterfall([
+      (done) => {
+        Category.find(done);
+      },
+      (data, done) => {
+        async.map(data[0].items, (item, cb) => {
+          Item.findById(item, (err, doc) => {
+            cb(null, doc);
+          });
+        }, done);
+      }], (err, data) => {
       if (err)
         return next(err);
-      res.status(200).send(doc);
+      res.send(data);
     });
   }
 
-  getItem(req, res, next) {
-    const itemId = req.params.itemId;
-    Item.findById(itemId, (err, doc) => {
+  getCategory(req, res, next) {
+    const categoryId = req.params.categoryId;
+    async.waterfall([
+      (done) => {
+        Category.findById(categoryId, done);
+      },
+      (data, done) => {
+        async.map(data.items, (item, cb) => {
+          Item.findById(item, (err, doc) => {
+            cb(null, doc);
+          });
+        }, done);
+      }], (err, data) => {
       if (err)
         return next(err);
-      res.status(200).send(doc);
+      res.send(data);
     });
   }
 
-  deleteItem(req, res, next) {
-    const itemId = req.params.itemId;
-    Item.remove({'_id': itemId}, (err, doc) => {
+  deleteCategory(req, res, next) {
+    const categoryId = req.params.categoryId;
+    Category.remove({'_id': categoryId}, (err, doc) => {
       if (err)
         return next(err);
       res.status(204).send(doc);
     });
   }
 
-  addItem(req, res, next) {
+  addCategory(req, res, next) {
     const data = req.body;
-    console.log(req.body);
-    new Item(data).save((err, doc) => {
+    new Category(data).save((err, doc) => {
       if (err)
         return next(err);
       res.status(201).send(doc);
     });
   }
 
-  updateItem(req, res, next) {
-    const itemId = req.params.itemId;
-    const name = req.body.name;
-    Item.update({'_id': itemId}, {name}, (err, doc) => {
+  updateCategory(req, res, next) {
+    const categoryId = req.params.categoryId;
+    const items = req.body.items;
+    Category.update({'_id': categoryId}, {items}, (err, doc) => {
       if (err)
         return next(err);
       res.status(204).send(doc);
     });
   }
+
+
 }
