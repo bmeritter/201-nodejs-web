e504d7463a3b72df91f0e088870e66bffba904cc
bmeritter <bme_ritter@foxmail.com>
2017-01-20 15:40:02 +0800

    refreshMongo

--- tools/refreshMongo.js
+++ tools/refreshMongo.js
@@ -1,3 +1,26 @@
-/**
- * Created by ritter on 17-1-18.
- */
+import mogoose from 'mongoose';
+import rawData from './fixture/raw-data';
+import Item from '../model/Item';
+import Category from '../model/Category';
+import Cart from '../model/Cart';
+
+const modelMap = {
+  Item,
+  Cart,
+  Category
+};
+
+let docs = Object.keys(rawData);
+
+mogoose.connect('mongodb://localhost/supermarket');
+
+Object.keys(rawData).forEach((v) => {
+  modelMap[v].remove(() => {
+    modelMap[v].create(rawData[v], () => {
+      docs = docs.filter(doc => doc !== v);
+      if (docs.length === 0) {
+        process.exit(0);
+      }
+    })
+  });
+});
