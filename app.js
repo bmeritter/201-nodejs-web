e4fbf858a53f2537c82bb923262cdf27f57ce404
bmeritter <bme_ritter@foxmail.com>
2017-01-20 15:38:45 +0800

    add bodyParser

--- app.js
+++ app.js
@@ -2,19 +2,23 @@ import mongoose from 'mongoose';
 import express from 'express';
 import config from 'config';
 import router from './router';
+import bodyParser from 'body-parser';
 
 mongoose.connect(config.get('mongoUri'));
 
 const app = express();
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', (req, res)=> {
-    res.send({
-        'hello': 'world'
-    })
-})
+app.get('/', (req, res) => {
+  res.send({
+    'hello': 'world'
+  })
+});
 
 router(app);
 
-app.listen(config.get('httpPort'), ()=> {
-    console.log('server started at http://localhost:' + config.get('httpPort'));   // eslint-disable-line no-console
-})
+app.listen(config.get('httpPort'), () => {
+  console.log('server started at http://localhost:' + config.get('httpPort'));   // eslint-disable-line no-console
+});
