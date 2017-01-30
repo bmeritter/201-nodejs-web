72ccd45ef002debac8d8d1384384dfe37f071613
bmeritter <bme_ritter@foxmail.com>
2017-01-30 18:47:03 +0800

    update raw-data

--- tools/fixture/raw-data.js
+++ tools/fixture/raw-data.js
@@ -2,22 +2,29 @@ export default {
   Item: [{
     '_id': '587f0f2586653d19297d40c2',
     name: '钢笔',
-    price: 12
+    price: 12,
+    categoryId: '587f0f2586653d19297d40c8'
   }, {
     '_id': '587f0f2586653d19297d40c3',
     name: '电脑',
-    price: 6000
+    price: 6000,
+    categoryId: '587f0f2586653d19297d40c9'
   }, {
     '_id': '587f0f2586653d19297d40c4',
     name: '手机',
-    price: 4000
+    price: 3000,
+    categoryId: '587f0f2586653d19297d40c9'
+  }],
+  Category: [{
+    '_id': '587f0f2586653d19297d40c8',
+    name: '文具'
   }, {
-    '_id': '587f0f2586653d19297d40c5',
-    name: '语文书',
-    price: 30
+    '_id': '587f0f2586653d19297d40c9',
+    name: '电子产品'
   }],
   Cart: [{
     '_id': '587f0f2586653d19297d40c6',
+    userId: '1',
     items: [
       {
         item: '587f0f2586653d19297d40c2',
@@ -31,14 +38,5 @@ export default {
         count: 1
       }
     ]
-  }],
-  Category: [{
-    '_id': '587f0f2586653d19297d40c8',
-    name: '文具',
-    items: ['587f0f2586653d19297d40c2']
-  }, {
-    '_id': '587f0f2586653d19297d40c9',
-    name: '电子产品',
-    items: ['587f0f2586653d19297d40c3', '587f0f2586653d19297d40c4']
   }]
 };
