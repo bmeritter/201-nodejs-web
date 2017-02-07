b84e26a1cd76646f666be0c6ce4f8aa39d90fce9
bmeritter <bme_ritter@foxmail.com>
2017-02-07 21:59:23 +0800

    add GET /carts/cartId

--- spec/cart-spec.js
+++ spec/cart-spec.js
@@ -18,4 +18,35 @@ describe('CartContronller', () => {
       .end(done);
   });
 
+  it('GET /carts/:cartId', (done) => {
+    request
+      .get('/carts/587f0f2586653d19297d40c6')
+      .expect(200)
+      .expect((res) => {
+        res.body.should.eql({
+          "_id": "587f0f2586653d19297d40c6",
+          "userId": "1",
+          "__v": 0,
+          "items": [
+            {
+              "item": "587f0f2586653d19297d40c2",
+              "count": 1,
+              "_id": "5899cce9d9fd8719fe673436"
+            },
+            {
+              "item": "587f0f2586653d19297d40c3",
+              "count": 1,
+              "_id": "5899cce9d9fd8719fe673435"
+            },
+            {
+              "item": "587f0f2586653d19297d40c4",
+              "count": 1,
+              "_id": "5899cce9d9fd8719fe673434"
+            }
+          ]
+        });
+      })
+      .end(done)
+  });
+
 });
