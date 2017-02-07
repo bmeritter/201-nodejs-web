3926af341c18e1ff0cc3b05bfd9c1d07607722b6
bmeritter <bme_ritter@foxmail.com>
2017-02-07 21:58:48 +0800

    updata raw-data

--- tools/fixture/raw-data.js
+++ tools/fixture/raw-data.js
@@ -27,13 +27,16 @@ module.exports = {
     userId: '1',
     items: [
       {
+        '_id': '5899cce9d9fd8719fe673436',
         item: '587f0f2586653d19297d40c2',
         count: 1
       }, {
+        '_id': '5899cce9d9fd8719fe673435',
         item: '587f0f2586653d19297d40c3',
         count: 1
       },
       {
+        '_id': '5899cce9d9fd8719fe673434',
         item: '587f0f2586653d19297d40c4',
         count: 1
       }
